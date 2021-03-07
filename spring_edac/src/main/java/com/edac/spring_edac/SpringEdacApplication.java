058fa5a4f0792f056e101ac76ca4b9d22afc1c17
Rajesh Mahajan <rmahajan16@outlook.com>
2021-03-07 22:39:54 +0530

    First commit

---
+++ spring_edac/src/main/java/com/edac/spring_edac/SpringEdacApplication.java
@@ -0,0 +1,13 @@
+package com.edac.spring_edac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEdacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEdacApplication.class, args);
+	}
+
+}
---
+++ spring_edac/src/test/java/com/edac/spring_edac/SpringEdacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edac.spring_edac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEdacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
